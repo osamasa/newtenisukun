ed7afec5dc909215d32d654f3c1b8b152fd69973
Masashi Osada <osamasa@gmail.com>
2020-02-12 18:07:32 +0900

    add router password

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSW9cH4jXyDdMpnyAvXJ_yoG6fo9PYJKs",
+    authDomain: "mytenisransuuhyoukunv3.firebaseapp.com",
+    databaseURL: "https://mytenisransuuhyoukunv3.firebaseio.com",
+    projectId: "mytenisransuuhyoukunv3",
+    storageBucket: "mytenisransuuhyoukunv3.appspot.com",
+    messagingSenderId: "556741671747",
+    appId: "1:556741671747:web:1e921cf2b0e247bb64cdf6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
     router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Game from '../components/HelloWorld'
+import Signup from '../components/Signup'
+import Signin from '../components/Signin'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/game/:curgameid',
-      name: 'game',
-      component: Game
-    }
-  ]
+    mode: 'history',
+    routes: [
+	{
+	    path: '/game/:curgameid',
+	    name: 'game',
+	    component: Game
+	},
+	{
+	    path: '/signup',
+	    name: 'Signup',
+	    component: Signup
+	},
+	{
+	    path: '/signin',
+	    name: 'Signin',
+	    component: Signin
+	}      
+    ]
 })
